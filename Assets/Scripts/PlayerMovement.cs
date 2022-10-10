c5e654bfb2f93e1e6007771473d69b62cb1ea9d6
Jeremy Hunton <jhunton22@gmail.com>
2022-10-10 12:47:59 -0400

    Set Player Movement to Last Direction Moved
    Set the player idle animation to stop in the direction the player last moved.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D body;
 
     Vector2 movement;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
@@ -17,6 +18,18 @@ public class PlayerMovement : MonoBehaviour
         // player movement input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // set last motion moved
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("LastMoveHor", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("LastMoveVert", Input.GetAxisRaw("Vertical"));
+        }
+
     }
 
     private void FixedUpdate()
